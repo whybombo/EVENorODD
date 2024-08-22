1603ebb67ac8a6248852822cf5406f536049947f
Blake Nelson <168068011+whybombo@users.noreply.github.com>
2024-08-22 03:30:55 -0500

    Add files via upload

---
+++ even odd.py
@@ -0,0 +1,10 @@
+print("Even or Odd Simulator")
+number = int(input("Enter an interger: "))
+
+remainder = number % 2
+
+if remainder == 0:
+    print("sick its even")
+else:
+    print("its odd i hate you")
+print("\n")
